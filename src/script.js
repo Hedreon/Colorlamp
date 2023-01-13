7b718fdfd773074af3e43e2ea0a88d2243cb5417
Hedreon <hedreon@e.email>
2023-01-13 19:34:01 +0000

    Update code to support the "rgb()" values
    rgb(69, 69, 69)

--- src/script.js
+++ src/script.js
@@ -7,6 +7,13 @@ document.addEventListener("DOMContentLoaded", function() {
         if (value.startsWith("#")) {
             // Input is in hexadecimal format
             document.body.style.backgroundColor = value;
+        } else if(value.startsWith("rgb(")) {
+            // Input is in vanila RGB format
+            var match = value.match(/rgb\((\d+),\s*(\d+),\s*(\d+)\)/);
+            if (match) {
+              var red = match[1], green = match[2], blue = match[3];
+              document.body.style.backgroundColor = "rgb("+red+","+green+","+blue+")";
+            }
         } else {
             // Input is in RGB format
             document.body.style.backgroundColor = "rgb(" + value + ")";
